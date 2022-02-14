f85e803ac1ec4f099102a7462cffef815406524e
junsub <skawns27@gmail.com>
2022-02-14 19:19:52 +0900

    2022-02-16:주문 서비스 및 패키지 설정

---
+++ Backend/Server/daeguro/src/main/java/com/daeguro/DaeguroApplication.java
@@ -0,0 +1,13 @@
+package com.daeguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeguroApplication.class, args);
+	}
+
+}
---
+++ Backend/Server/daeguro/src/test/java/com/daeguro/DaeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
